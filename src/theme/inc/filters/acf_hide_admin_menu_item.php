dd5e631775333a0224c8866d82714884c5925830
Guillo <guillermobaron@gmail.com>
2024-05-24 16:20:42 -0300

    fix(filters): rename filter

--- src/theme/inc/filters/acf_hide_admin_menu_item.php
+++ src/theme/inc/filters/acf_hide_admin_menu_item.php
@@ -4,10 +4,10 @@
 // Hide the ACF Admin Menu Item
 //==============================================================================
 
-if (!function_exists("talampaya_hide_acf_admin_menu_item")):
-	function talampaya_hide_acf_admin_menu_item($more)
+if (!function_exists("talampaya_acf_hide_admin_menu_item")):
+	function talampaya_acf_hide_admin_menu_item($more)
 	{
 		return false;
 	}
-	add_filter("acf/settings/show_admin", "talampaya_hide_acf_admin_menu_item");
+	add_filter("acf/settings/show_admin", "talampaya_acf_hide_admin_menu_item");
 endif;
