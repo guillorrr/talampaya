77492b83e118df84d0e2e85aec3186892c898705
Guillo <guillermobaron@gmail.com>
2024-06-26 20:42:25 -0300

    feat(timber): update namespace & rework theme files

--- src/theme/author.php
+++ src/theme/author.php
@@ -1,21 +1,20 @@
 <?php
+
 /**
  * The template for displaying Author Archive pages
  *
  * Methods for TimberHelper can be found in the /lib sub-directory
  *
- * @package  WordPress
- * @subpackage  Timber
- * @since    Timber 0.1
  */
 
-global $wp_query;
+namespace App;
+
+use Timber\Timber;
 
 $context = Timber::context();
-$context["posts"] = Timber::get_posts();
-if (isset($wp_query->query_vars["author"])) {
-	$author = Timber::get_user($wp_query->query_vars["author"]);
-	$context["author"] = $author;
-	$context["title"] = "Author Archives: " . $author->name();
+
+if (isset($context["author"])) {
+	$context["title"] = sprintf(__("Archive of %s", "talampaya"), $context["author"]->name());
 }
+
 Timber::render(["pages/author.twig", "pages/archive.twig"], $context);
