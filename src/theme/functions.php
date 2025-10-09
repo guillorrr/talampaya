68e2fa25cdd6f69a428fbadf9a4742799a7d6b76
Guillo <guillermobaron@gmail.com>
2025-10-09 10:37:29 -0300

    refactor(theme): simplify Timber template directory configuration and clean up unused paths

--- src/theme/functions.php
+++ src/theme/functions.php
@@ -13,8 +13,7 @@ require_once __DIR__ . "/../../vendor/autoload.php";
 
 Timber::init();
 
-$theme_dir = get_template_directory();
-Timber::$dirname = ["{$theme_dir}/templates", "{$theme_dir}/views"];
+Timber::$dirname = [get_template_directory() . "/views"];
 
 if (class_exists("App\\Core\\TalampayaBase")) {
 	new TalampayaBase();
@@ -45,18 +44,7 @@ if (class_exists("ACF")) {
 	require_once __DIR__ . "/src/Features/Acf/acf.php";
 }
 
-$directories = [
-	__DIR__ . "/hooks/Filters",
-	__DIR__ . "/src/Core/Config",
-	//    __DIR__ . "/app/models",
-	//    __DIR__ . "/app/services",
-	//    __DIR__ . "/app/defaults",
-	//	__DIR__ . "/app/controllers",
-	//    __DIR__ . "/app/endpoints",
-	//	__DIR__ . "/app/helpers",
-	//	__DIR__ . "/utils",
-	//    __DIR__ . "/app/cron",
-];
+$directories = [__DIR__ . "/hooks/Filters", __DIR__ . "/src/Core/Config"];
 
 foreach ($directories as $dir) {
 	$files = FileUtils::talampaya_directory_iterator($dir);
