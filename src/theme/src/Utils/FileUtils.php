b9b28c7ada45d90fd904d4735ec69d89f13ca140
Guillo <guillermobaron@gmail.com>
2025-10-10 09:21:04 -0300

    docs(utils): add phpdoc comments for file and directory iterator methods

--- src/theme/src/Utils/FileUtils.php
+++ src/theme/src/Utils/FileUtils.php
@@ -6,12 +6,19 @@ use DirectoryIterator;
 
 class FileUtils
 {
-	// -----------------------------------------------------------------------------
-	// Directory Iterator Group By Folder
-	// -----------------------------------------------------------------------------
+	/**
+	 * Directory Iterator Group by Folder
+	 *
+	 * Iterates over subdirectories in a specified path, and for each subdirectory,
+	 * it includes PHP files and merges their returned arrays into a grouped array.
+	 *
+	 * @param string $path The directory path to iterate over. Defaults to "/inc/register".
+	 * @param string $extension The file extension to filter by. Defaults to "php".
+	 * @return array An associative array where keys are subdirectory names and values are arrays of merged data from included files.
+	 */
 	public static function talampaya_directory_iterator_group_by_folder(
-		$path = "/inc/register",
-		$extension = "php"
+		string $path = "/inc/register",
+		string $extension = "php"
 	): array {
 		$data = [];
 		foreach ($directories = new DirectoryIterator($path) as $directory) {
@@ -33,15 +40,23 @@ class FileUtils
 		return $data;
 	}
 
-	// -----------------------------------------------------------------------------
-	// Directory Iterator
-	// -----------------------------------------------------------------------------
-
+	/**
+	 * Directory Iterator
+	 *
+	 * Iterates over files in a specified directory and filters them based on their extension,
+	 * prefix, and a list of excluded file names.
+	 *
+	 * @param string $path The directory path to iterate over. Defaults to "/inc/filters".
+	 * @param string $extension The file extension to filter by. Defaults to "php".
+	 * @param string $prefix_exclude The filename prefix to exclude. Defaults to "_".
+	 * @param array $exclude_files An array of filenames (without extensions) to exclude. Defaults to an empty array.
+	 * @return array An array of file paths that match the filtering criteria.
+	 */
 	public static function talampaya_directory_iterator(
-		$path = "/inc/filters",
-		$extension = "php",
-		$prefix_exclude = "_",
-		$exclude_files = []
+		string $path = "/inc/filters",
+		string $extension = "php",
+		string $prefix_exclude = "_",
+		array $exclude_files = []
 	): array {
 		$data = [];
 		foreach ($files = new DirectoryIterator($path) as $file) {
