0664408fb9eb6bd8a35fc5d2189e74393052d061
Guillo <guillermobaron@gmail.com>
2025-10-10 08:39:07 -0300

    refactor: add phpdoc blocks and improve type hinting for helpers

--- src/theme/src/Inc/Helpers/AttachmentsHelper.php
+++ src/theme/src/Inc/Helpers/AttachmentsHelper.php
@@ -4,7 +4,14 @@ namespace App\Inc\Helpers;
 
 class AttachmentsHelper
 {
-	public static function get_image_id_by_filename($filename, $debug = false): ?int
+	/**
+	 * Get the image ID by filename.
+	 *
+	 * @param string $filename The filename to search for.
+	 * @param bool $debug Whether to output debug information.
+	 * @return int|null The image ID if found, null otherwise.
+	 */
+	public static function get_image_id_by_filename(string $filename, bool $debug = false): ?int
 	{
 		global $wpdb;
 
@@ -39,11 +46,20 @@ class AttachmentsHelper
 		return $image_id ? intval($image_id) : null;
 	}
 
+	/**
+	 * Set the post thumbnail if the image exists, otherwise sideload it.
+	 *
+	 * @param int $post_id The post ID to set the thumbnail for.
+	 * @param string $image_url The URL of the image to use as the thumbnail.
+	 * @param string $title The title to use for the image if sideloaded.
+	 * @param bool $debug Whether to output debug information.
+	 * @return void
+	 */
 	public static function set_post_thumbnail_if_exists(
-		$post_id,
-		$image_url,
-		$title,
-		$debug = false
+		int $post_id,
+		string $image_url,
+		string $title,
+		bool $debug = false
 	): void {
 		if (!empty($image_url)) {
 			if ($debug) {
