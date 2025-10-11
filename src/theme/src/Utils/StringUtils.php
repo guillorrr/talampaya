703248f6f3c56c43fce8e158fcd1b7b2e44978ef
Guillo <guillermobaron@gmail.com>
2025-10-11 14:53:23 -0300

    feat(utils): add talampaya_make_phrase_ucfirst for title casing phrases

--- src/theme/src/Utils/StringUtils.php
+++ src/theme/src/Utils/StringUtils.php
@@ -28,4 +28,23 @@ class StringUtils
 
 		return $minify;
 	}
+
+	public static function talampaya_make_phrase_ucfirst($phrase): string|null
+	{
+		if (empty($phrase)) {
+			//error_log("talampaya_make_phrase_ucfirst: Título vacío");
+			return null;
+		}
+
+		// Verificar si el título está todo en mayúsculas o todo en minúsculas
+		if (
+			$phrase === mb_strtoupper($phrase, "UTF-8") ||
+			$phrase === mb_strtolower($phrase, "UTF-8")
+		) {
+			$phrase = ucfirst(mb_strtolower($phrase, "UTF-8"));
+			//error_log("talampaya_make_phrase_ucfirst: Título convertido a formato ucfirst");
+		}
+
+		return $phrase;
+	}
 }
