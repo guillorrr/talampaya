1474bc3c7d8e6a5c1729385f3abee1078972e9e6
Guillo <guillermobaron@gmail.com>
2025-12-08 16:30:45 -0300

    feat: prioritize custom block category in block editor

--- src/theme/src/Hooks/Filters/blocks_add_custom_category.php
+++ src/theme/src/Hooks/Filters/blocks_add_custom_category.php
@@ -2,12 +2,15 @@
 
 function blocks_add_custom_category($categories, $post): array
 {
-	return array_merge($categories, [
+	// Add custom category at the beginning
+	$custom_category = [
 		[
 			"slug" => "talampaya",
 			"title" => __("Talampaya", "talampaya"),
 			"icon" => "marker",
 		],
-	]);
+	];
+
+	return array_merge($custom_category, $categories);
 }
 add_filter("block_categories_all", "blocks_add_custom_category", 10, 2);
